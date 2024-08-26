0addb6f47186c0751cf304f31333d2422cd063a4
estanislao blatter <blatter16estanislao@gmail.com>
2024-08-26 19:25:11 -0300

    agregue un nuevo proyecto y mas lineas

--- ejercicio 3/Program.cs
+++ ejercicio 3/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ejercicio_3
+﻿using ejercicio3.entidades;
+
+namespace ejercicio_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Ingrese el valor de diagonal mayor: ");
+            var DM = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de diagonal menor: ");
+            var dm = int.Parse(Console.ReadLine());
+            Rombo r = new Rombo(DM,dm);
+            Console.WriteLine(r.GetInfo());
         }
     }
 }
---
+++ ejercicio3.entidades/Rombo.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ejercicio3.entidades
+{
+    public struct Rombo
+    {
+        public int DM {  get; set; }
+        public int dm { get; set; }
+        public Rombo(int diagMayor,int diagMenor) 
+        {
+          DM = diagMayor;
+          dm = diagMenor;
+        } 
+        public double GetLadoRombo=>Math.Sqrt(Math.Pow(DM/2,2)+Math.Pow(dm/2,2));
+        public double GetPerimetro => GetLadoRombo * 4;
+        public double GetSuperficie => (DM * dm) / 2;
+        public string GetInfo()
+        {
+            StringBuilder sb=new StringBuilder();
+            sb.AppendLine($"Rombo de DM:{DM}");
+            sb.AppendLine($"Rombo de dm:{dm}");
+            sb.AppendLine($"Valor del lado:{GetLadoRombo}");
+            sb.AppendLine($"Superficie:{GetSuperficie}");
+            sb.AppendLine($"Perimetro:{GetPerimetro}");
+
+            return sb.ToString();
+        }
+    }
+}
